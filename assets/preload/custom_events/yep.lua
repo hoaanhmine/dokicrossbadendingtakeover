ba3401b97d89ecf63a7d66692279013641341e1c
hoaanhmine <hoaminecraftpc@gmail.com>
2022-08-26 16:39:21 +0700

    fix stage

--- assets/preload/custom_events/yep.lua
+++
@@ -1,9 +0,0 @@
-function onEvent(n,v1,v2)
-	if n == 'yep' then
-		if v1 == 'true' then
-		 setObjectOrder('boyfriendGroup', getObjectOrder('gfGroup')
-		else
-		setObjectOrder('boyfriendGroup', getObjectOrder('boyfriendGroup')
-		end	
-	end 
-end
